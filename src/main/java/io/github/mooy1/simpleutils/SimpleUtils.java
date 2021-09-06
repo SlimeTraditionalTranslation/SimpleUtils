97b5b60e00866676253dc3785987277a3ec2ae5b
weiiii <joegane0629@gmail.com>
2021-09-07 04:43:18 +0800

    把pom指向正確的位子

--- src/main/java/io/github/mooy1/simpleutils/SimpleUtils.java
+++ src/main/java/io/github/mooy1/simpleutils/SimpleUtils.java
@@ -5,8 +5,8 @@ import java.io.File;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
-//import io.github.mooy1.infinitylib.core.AbstractAddon;
-import io.github.mooy1.infinitylib.metrics.bukkit.Metrics;
+import io.github.mooy1.infinitylib.core.AbstractAddon;
+//import io.github.mooy1.infinitylib.metrics.bukkit.Metrics;
 //import io.github.mooy1.infinitylib.metrics.charts.SimplePie;
 import io.github.mooy1.simpleutils.implementation.Items;
 
@@ -27,7 +27,7 @@ public final class SimpleUtils extends AbstractAddon {
         //Metrics metrics = new Metrics(this, 10285);
         String ixInstalled = String.valueOf(getServer().getPluginManager().isPluginEnabled("InfinityExpansion"));
         //String autoUpdates = String.valueOf(autoUpdatesEnabled());
-        metrics.addCustomChart(new SimplePie("ix_installed", () -> ixInstalled));
+        //metrics.addCustomChart(new SimplePie("ix_installed", () -> ixInstalled));
         //metrics.addCustomChart(new SimplePie("auto_updates", () -> autoUpdates));
     }
 
